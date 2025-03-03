ccc5be8f4627c123662fdf87cd0cfa942362dcf3
LongVu99 <dinhlong0762@gmail.com>
2025-03-03 08:55:20 +0700

    update base-js

--- js/base.js
+++ js/base.js
@@ -11,9 +11,27 @@ $(document).ready(function(){
             slidesToScroll: slidesToScroll,
             arrows: true,
             dots: true,
-            Infinity: true,
+            infinity: true,
             autoplay: true,
             autoplaySpeed: 3000,
+
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3,
+                    }
+                },
+
+                {
+                    breakpoint: 760,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2,
+                    }
+                }
+            ]
 
         })
     })
