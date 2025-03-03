a092e9e061ee201c1f94675516d73ca6b424fd8e
quanghacker97 <quanghacker97@gmail.com>
2025-03-03 10:09:58 +0700

    up header

--- header.php
+++ header.php
@@ -1,1 +1,168 @@
+<!DOCTYPE html>
+<html lang="vi">
 
+<head>
+    <link rel="stylesheet" href="css/header.css">
+</head>
+
+<body>
+
+    <header>
+        <div class="top-bar">
+            <div class="logo">Hupuna</div>
+            <div class="search-box d-flex flex-row">
+                <input type="text" placeholder="Tìm kiếm từ khóa, thông tin, công dụng...">
+                <button type="button">Search</button>
+            </div>
+            <div class="user-options">
+                <span>0889.363.889</span>
+                <button class="login-btn">Đăng Nhập</button>
+                <button class="cart">Giỏ hàng</button>
+            </div>
+        </div>
+        <nav class="main-menu">
+            <ul>
+                <li class="dropdown">
+                    <button class="menu-btn">☰ DANH MỤC SẢN PHẨM</button>
+                    <div class="mega-menu">
+                        <div class="menu-left">
+                            <ul>
+                                <li data-menu="menu1">
+                                    <a href="#">HỘP CARTON SÓNG THÔNG DỤNG</a>
+                                </li>
+                                <li data-menu="menu2"><a href="#">HỘP OFFSET MỀM CAO CẤP</a></li>
+                                <li data-menu="menu3"><a href="#">HỘP CARTON CỨNG CAO CẤP</a></li>
+                                <li data-menu="menu4"><a href="#">SẢN PHẨM IN NHANH</a></li>
+                                <li data-menu="menu5"><a href="#">KHAY ĐỊNH HÌNH</a></li>
+                                <li data-menu="menu6"><a href="#">TÚI GIẤY</a></li>
+                                <li data-menu="menu7"><a href="#">PHỤ KIỆN ĐÓNG HÀNG</a></li>
+                                <li data-menu="menu8"><a href="#">XEM TẤT CẢ</a></li>
+                            </ul>
+                        </div>
+                        <div class="menu-right">
+                            <div id="menu1" class="submenu">
+                                <div class="row">
+                                    <div class="col">
+                                        THEO CẤU TẠO
+                                    </div>
+                                    <div class="col">
+                                        THEO NGÀNH NGHỀ
+                                    </div>
+                                    <div class="col">
+                                        Column
+                                    </div>
+                                </div>
+                            </div>
+                            <div id="menu2" class="submenu">
+                                <h3>Hộp Offset Mềm</h3>
+                                <p>Danh sách hộp offset mềm cao cấp...</p>
+                            </div>
+                            <div id="menu3" class="submenu">
+                                <h3>Hộp Carton Cứng</h3>
+                                <p>Hộp carton cứng cao cấp, bền đẹp...</p>
+                            </div>
+                            <div id="menu4" class="submenu">
+                                <h3>Sản Phẩm In Nhanh</h3>
+                                <p>Dịch vụ in nhanh chất lượng cao...</p>
+                            </div>
+                            <div id="menu5" class="submenu">
+                                <h3>Khay Định Hình</h3>
+                                <p>Khay định hình cho sản phẩm đặc biệt...</p>
+                            </div>
+                            <div id="menu6" class="submenu">
+                                <h3>Túi Giấy</h3>
+                                <p>Các loại túi giấy sang trọng...</p>
+                            </div>
+                            <div id="menu7" class="submenu">
+                                <h3>Phụ Kiện Đóng Hàng</h3>
+                                <p>Danh sách phụ kiện hỗ trợ đóng gói...</p>
+                            </div>
+                            <div id="menu8" class="submenu">
+                                <h3>Xem Tất Cả</h3>
+                                <p>Tất cả sản phẩm tại cửa hàng...</p>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li><a href="#">TRANG CHỦ</a></li>
+                <li><a href="#">DỊCH VỤ IN ẤN ▼</a></li>
+                <li><a href="#">VỀ CHÚNG TÔI</a></li>
+                <li><a href="#">TIN TỨC</a></li>
+                <li class="highlight"><a href="#">📹 VIDEO NỔI BẬT</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <script src="js/header.js"></script>
+</body>
+
+</html>
+<!-- 
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <link rel="stylesheet" href="css/header.css">
+</head>
+<body> -->
+
+<!-- <header>
+    <div class="top-bar">
+        <div class="logo">Hupuna</div>
+        <button class="menu-btn">☰ DANH MỤC SẢN PHẨM</button>
+    </div>
+
+    <nav class="mega-menu">
+        <div class="menu-left">
+            <ul>
+                <li data-menu="menu1">HỘP CARTON SÓNG THÔNG DỤNG</li>
+                <li data-menu="menu2">HỘP OFFSET MỀM CAO CẤP</li>
+                <li data-menu="menu3">HỘP CARTON CỨNG CAO CẤP</li>
+                <li data-menu="menu4">SẢN PHẨM IN NHANH</li>
+                <li data-menu="menu5">KHAY ĐỊNH HÌNH</li>
+                <li data-menu="menu6">TÚI GIẤY</li>
+                <li data-menu="menu7">PHỤ KIỆN ĐÓNG HÀNG</li>
+                <li data-menu="menu8">XEM TẤT CẢ</li>
+            </ul>
+        </div>
+
+        <div class="menu-right">
+            <div id="menu1" class="submenu">
+                <h3>Hộp Carton Sóng</h3>
+                <p>Danh sách các loại hộp carton sóng thông dụng...</p>
+            </div>
+            <div id="menu2" class="submenu">
+                <h3>Hộp Offset Mềm</h3>
+                <p>Danh sách hộp offset mềm cao cấp...</p>
+            </div>
+            <div id="menu3" class="submenu">
+                <h3>Hộp Carton Cứng</h3>
+                <p>Hộp carton cứng cao cấp, bền đẹp...</p>
+            </div>
+            <div id="menu4" class="submenu">
+                <h3>Sản Phẩm In Nhanh</h3>
+                <p>Dịch vụ in nhanh chất lượng cao...</p>
+            </div>
+            <div id="menu5" class="submenu">
+                <h3>Khay Định Hình</h3>
+                <p>Khay định hình cho sản phẩm đặc biệt...</p>
+            </div>
+            <div id="menu6" class="submenu">
+                <h3>Túi Giấy</h3>
+                <p>Các loại túi giấy sang trọng...</p>
+            </div>
+            <div id="menu7" class="submenu">
+                <h3>Phụ Kiện Đóng Hàng</h3>
+                <p>Danh sách phụ kiện hỗ trợ đóng gói...</p>
+            </div>
+            <div id="menu8" class="submenu">
+                <h3>Xem Tất Cả</h3>
+                <p>Tất cả sản phẩm tại cửa hàng...</p>
+            </div>
+        </div>
+    </nav>
+</header>
+
+<script src="js/header.js"></script>
+
+</body>
+</html> -->
