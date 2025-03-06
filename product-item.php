9adb5e77a75a52122e182330704c05ebf4d19908
Trần Văn Thế <135038699+tranvanthe11@users.noreply.github.com>
2025-03-06 09:42:32 +0700

    update

--- product-item.php
+++ product-item.php
@@ -1,39 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="./css/base-style.css" rel="stylesheet">
-    <link href="./css/product-item.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script>
-    <title>Document</title>
-</head>
-
-<body>
-    <div class="productItem">
-        <div class="img-100 imgProduct">
-            <img src="./images/bannerhopcarton.jfif" alt="Product 1">
-        </div>
-        <div class="contentProduct mt-2">
-            <p class="btn btn-danger fs-10">Giảm 1.000đ</p>
-            <p class="fw-bolder fs-15 line-clamp">Thùng xốp đại kích thước 70 50 44 cm nuôi cá
-            </p>
-            <p class="text-danger">3.594 VNĐ <span class="btn btn-danger fs-10">25%</span></p>
-            <div class="d-flex align-items-center">
-                <img class="star" src="./icons/starblue.svg" alt="star">
-                <img class="star" src="./icons/starblue.svg" alt="star">
-                <img class="star" src="./icons/starblue.svg" alt="star">
-                <img class="star" src="./icons/starblue.svg" alt="star">
-                <img class="star" src="./icons/star.svg" alt="star">
-                <span class="text-primary fs-10">Đã bán 200 SP</span>
-            </div>
+<div class="productItem">
+    <div class="img-100 imgProduct">
+        <img src="./images/bannerhopcarton.jfif" alt="Product 1">
+    </div>
+    <div class="contentProduct mt-2">
+        <p class="btn btn-danger fs-10">Giảm 1.000đ</p>
+        <p class="fw-bolder fs-15 line-clamp">Thùng xốp đại kích thước 70 50 44 cm nuôi cá
+        </p>
+        <p class="text-danger">3.594 VNĐ <span class="btn btn-danger fs-10">25%</span></p>
+        <div class="d-flex align-items-center">
+            <img class="star" src="./icons/starblue.svg" alt="star">
+            <img class="star" src="./icons/starblue.svg" alt="star">
+            <img class="star" src="./icons/starblue.svg" alt="star">
+            <img class="star" src="./icons/starblue.svg" alt="star">
+            <img class="star" src="./icons/star.svg" alt="star">
+            <span class="text-primary fs-10">Đã bán 200 SP</span>
         </div>
     </div>
-</body>
-
-</html>
+</div>
