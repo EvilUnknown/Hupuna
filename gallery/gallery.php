b291a5b544ae41e7f23eaa3d93fdc4c76212fc3c
LOD3008 <89307456+LOD3008@users.noreply.github.com>
2025-03-10 17:43:16 +0700

    gallery

---
+++ gallery/gallery.php
@@ -0,0 +1,57 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="css/bootstrap-grid.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+
+    <!-- Flipbook StyleSheet -->
+    <link href="css/dflip.min.css" rel="stylesheet" type="text/css">
+
+    <!-- Icons Stylesheet -->
+    <link href="css/themify-icons.min.css" rel="stylesheet" type="text/css">
+
+    <link rel="stylesheet" href="css/base-style.css">
+
+    <!-- jQuery library -->
+    <script src="../dflip/js/libs/jquery.min.js" type="text/javascript"></script>
+    <script src="../dflip/js/dflip.min.js" type="text/javascript"></script>
+
+    <title>Hupuna</title>
+
+</head>
+
+<body>
+    <?php include 'header.php'; ?>
+
+    <section>
+        <div class="_df_thumb" source="pdfs/cataloc-hop-banh-copy-3.pdf"
+          thumb="https://js.dearflip.com/wp-content/uploads/2023/07/lifestyle-magazine.jpeg">
+          <div class="df-book-wrapper">
+            <div class="df-book-page1"></div>
+            <div class="df-book-page2"></div>
+            <div class="df-book-cover df-icon-play-popup"><img width="210px" height="297px" class=""
+                alt="LifeStyle Magazine"
+                src="https://js.dearflip.com/wp-content/uploads/2023/07/lifestyle-magazine.jpeg" initid="11"><span
+                class="df-book-title">LifeStyle Magazine</span></div>
+          </div>
+        </>
+    </section>
+
+    <?php include 'footer.php'; ?>
+
+    <!-- <script src="js/base.js"></script> -->
+    <!-- <script src="js/page_home.js"></script> -->
+</body>
+
+</html>
+
+
+
