d2fbe5291643993f27ce0c290354cd6ae7187821
LOD3008 <89307456+LOD3008@users.noreply.github.com>
2025-03-10 12:03:04 +0700

    news_detail

--- single.php
+++ single.php
@@ -1,1 +1,97 @@
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="css/bootstrap-grid.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" type="text/css" href="css/slick.css"/>
+
+    <link rel="stylesheet" href="css/base-style.css">
+    <link rel="stylesheet" href="css/news-single.css">
+
+    <!-- jQuery library -->
+    <script src="js/jquery.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <!-- <script src="js/bootstrap.bundle.min.js"></script> -->
+    <script type="text/javascript" src="js/slick.min.js"></script>
+
+    <title>Hupuna</title>
+
+</head>
+
+<body>
+    <?php include 'header.php'; ?>
+
+    <section class="news-detail">
+        <div class="container">
+            <div class="news-detail-inner overflow-hidden">
+                <h1>Tai nghe chụp tai JBL Tune 670NC thông minh, tiện lợi, đáng mua vô cùng</h1>
+                <div class="time-post d-flex align-items-center fs-12 pb-3 pt-2">
+                    <div class="user-post fw-600">
+                        Nguyễn Thành
+                    </div>
+                    <div class="time">
+                        14 giờ trước
+                    </div>
+                </div>
+                <div class="avt mb-4">
+                    <img src="https://cdnv2.tgdd.vn/mwg-static/common/News/1575375/JBL-Tune-670NC-2.jpg" alt="">
+                </div>
+                <div class="news-content">
+                    <p>Bạn đang tìm kiếm một chiếc tai nghe không dây có khả năng "cách ly" bạn khỏi thế giới ồn ào xung quanh, mang đến trải nghiệm âm thanh sống động và thoải mái? Tai nghe chụp tai JBL Tune 670NC chính là một lựa chọn đáng cân nhắc, một ứng cử viên sáng giá với sự kết hợp hoàn hảo giữa chất âm ấn tượng, tính năng hiện đại và mức giá dễ tiếp cận. Trong bài viết này, mình sẽ chia sẻ những cảm nhận về chiếc tai nghe này, giúp bạn có cái nhìn rõ ràng hơn trước khi đưa ra quyết định mua sắm.</p>
+                    <h2>Vì sao tai nghe chụp tai là lựa chọn đáng tiền?</h2>
+                    <img src="https://cdnv2.tgdd.vn/mwg-static/common/News/1575375/JBL-Tune-670NC-7.jpg" alt="">
+                    <p>Khác với tai nghe in-ear nhỏ gọn hay earbud tiện lợi, tai nghe chụp tai như JBL Tune 670NC mang đến một trải nghiệm âm thanh hoàn toàn khác biệt. Thiết kế ôm trọn vành tai giúp tạo ra một không gian âm thanh kín đáo, giảm thiểu tối đa tiếng ồn từ môi trường xung quanh. Điều này không chỉ giúp bạn tập trung hơn vào âm nhạc mà còn bảo vệ thính giác tốt hơn, vì bạn không cần phải tăng âm lượng quá lớn để át đi tiếng ồn.</p>
+                    <p>Hơn nữa, kích thước lớn của tai nghe chụp tai cho phép các nhà sản xuất trang bị driver (màng loa) lớn hơn, mang lại âm bass sâu và mạnh mẽ hơn, dải âm rộng hơn và chi tiết âm thanh tốt hơn. Với JBL Tune 670NC, mình cảm nhận rõ sự khác biệt này khi thưởng thức những bản nhạc EDM sôi động hay những bản ballad trữ tình.</p>
+                    <h3>Tai nghe JBL Tune 670NC chất âm vượt trội trong tầm giá</h3>
+                    <h4>Hơn nữa, kích thước lớn của tai nghe chụp tai cho phép các nhà sản xuất trang bị driver</h4>
+                </div>
+
+                <div class="news-related mt-5">
+                    <div class="title fs-18 text-uppercase fw-700 mb-4">
+                        Bài viết liên quan
+                    </div>
+                    <div class="row">
+                        <?php for($i = 0; $i < 6; $i++) { ?>
+                            <div class="col-6 col-sm-4 col-lg-4">
+                                <div class="item mb-3">
+                                    <a href="" title="" class="wrap-img img-100">
+                                        <img src="images/product-item1.png" alt="">
+                                    </a>
+                                    <div class="my-2">
+                                        <a href="" title="" class="name fs-16 fw-500 my-2 text-black text-line-clamp-3">
+                                            Tai nghe TWS JBL Wave Beam 2: Lựa chọn chất âm tốt trong một mức giá hợp lý
+                                        </a>
+                                    </div>
+                                    
+                                    <div class="created fs-13">
+                                        19/01/2025
+                                    </div>
+                                </div>
+                            </div>
+                        <?php } ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <?php include 'footer.php'; ?>
+
+    <!-- <script src="js/base.js"></script> -->
+    <!-- <script src="js/page_home.js"></script> -->
+</body>
+
+</html>
+
+
+
